1cd92f6e7f23cc86f63c88a34bad021f6ca456b3
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-04-20 18:04:45 +0200

    Update callback.js

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -1,35 +1,68 @@
-export const handler = async (event) => {
+// netlify/functions/callback.js
+exports.handler = async (event) => {
   const { code } = event.queryStringParameters;
   const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 
-  const response = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
-    body: JSON.stringify({
-      client_id: GITHUB_CLIENT_ID,
-      client_secret: GITHUB_CLIENT_SECRET,
-      code,
-    }),
-  });
+  if (!code) {
+    return { statusCode: 400, body: "Missing code parameter" };
+  }
 
-  const data = await response.json();
+  try {
+    const response = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        client_id: GITHUB_CLIENT_ID,
+        client_secret: GITHUB_CLIENT_SECRET,
+        code,
+      }),
+    });
 
-  if (data.access_token) {
-    // Inyecta el token en una ventana popup que Decap CMS escucha
-    const script = `
-      <script>
-        window.opener.postMessage(
-          'authorization:github:success:${JSON.stringify({ token: data.access_token, provider: "github" })}',
-          '*'
-        );
-      </script>
-    `;
+    const data = await response.json();
+
+    if (data.access_token) {
+      const content = JSON.stringify({
+        token: data.access_token,
+        provider: "github",
+      });
+
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/html" },
+        body: `<!doctype html>
+<html>
+<head><title>Autenticando...</title></head>
+<body>
+<script>
+  (function() {
+    window.opener.postMessage(
+      'authorization:github:success:${content}',
+      window.location.origin
+    );
+    window.close();
+  })();
+<\/script>
+</body>
+</html>`,
+      };
+    }
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/html" },
-      body: script,
+      body: `<!doctype html><html><body><script>
+  window.opener.postMessage('authorization:github:error:{"message":"Token error"}', window.location.origin);
+  window.close();
+<\/script></body></html>`,
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Error: ${err.message}`,
     };
   }
-
-  return { statusCode: 401, body: "OAuth error" };
 };
